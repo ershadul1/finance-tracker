915c24a8eac4e51bd15a356b4ba1fe3231fc9cc2
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-16 20:11:51 +0600

    Add methods in transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,62 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @transactions = Transaction.all
+  end
+
+  
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+
+  
+  def create
+    @transaction = current_user.transactions.build(transaction_params)
+
+    respond_to do |format|
+      if @transaction.save
+        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
+        format.json { render :show, status: :created, location: @transaction }
+      else
+        format.html { render :new }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+    respond_to do |format|
+      if @transaction.update(transaction_params)
+        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
+        format.json { render :show, status: :ok, location: @transaction }
+      else
+        format.html { render :edit }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    respond_to do |format|
+      format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def transaction_params
+      params.require(:transaction).permit(:name, :amount, :group_id)
+    end
 end
