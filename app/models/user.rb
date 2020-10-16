60736a493e743748b9ae7d6fcba387bbb23e7840
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-16 18:13:10 +0600

    Add associations between user, transaction and group model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :transactions, foreign_key: :author_id
 end
