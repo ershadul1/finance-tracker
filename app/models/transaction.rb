a7e29656ab45c6c35426c95e7042f795b1402e33
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-17 17:39:19 +0600

    Add total_spent and total_earned scopes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,7 @@ class Transaction < ApplicationRecord
   validates :name, presence: true, length: { in: 2..50 }
   validates :amount, presence: true
   belongs_to :author, class_name: 'User'
+
+  scope :total_spent, -> { where(earning: false).sum(:amount) }
+  scope :total_earned, -> { where(earning: true).sum(:amount) }
 end
