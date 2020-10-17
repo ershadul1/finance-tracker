0d36cd341abe560e7f5dba41a4de16efe4d58140
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-17 23:49:37 +0600

    Add default image for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,15 @@ class Group < ApplicationRecord
 
   has_many :groups_transactions, foreign_key: :transaction_grouper_id
   has_many :grouped_transactions, through: :groups_transactions
+
+  after_commit :add_default_picture, on: %i[create update]
+
+  private
+
+  def add_default_picture
+    return if picture.attached?
+
+    picture.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-image.jpg')),
+                   filename: 'default.jpg', content_type: 'image/jpg')
+  end
 end
