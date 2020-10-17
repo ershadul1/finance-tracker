83414f70aff4c70c53d79dadcf4933faf0f983fe
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-17 16:49:05 +0600

    Update validations of groups and transactions

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   has_many :transactions
   has_one_attached :picture
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 2..50 }
   validates :amount, presence: true
   belongs_to :author, class_name: 'User'
 end
