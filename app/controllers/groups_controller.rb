d757f980fb99f4f23092c98f7df8ef2156615582
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-17 16:40:07 +0600

    Add picture for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -56,6 +56,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, :picture)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   has_many :transactions
+  has_one_attached :picture
 end
