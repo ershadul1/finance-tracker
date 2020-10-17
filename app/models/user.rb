4de670065ef553c99c847af23bef1eee2a9d1e70
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-17 23:49:14 +0600

    Add default image for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
 
   has_many :transactions, foreign_key: :author_id, dependent: :destroy
   has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-user.jpg')),
+                  filename: 'default.jpg', content_type: 'image/jpg')
+  end
 end
