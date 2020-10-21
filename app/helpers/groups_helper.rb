125ec9a230a82f8b0ff52a3ee4df02130ee615fb
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-21 08:43:03 +0600

    Add helper method for image

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+  def group_image(arg)
+    if arg.picture.attached?
+      content_tag(:span) do
+        image_tag arg.picture.variant(resize_to_limit: [500, 500]), class: 'group-image'
+      end
+    else
+      image_tag '/assets/default-image.jpg', class: 'group-image'
+    end
+  end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,11 @@
 module TransactionsHelper
+  def transaction_image(arg)
+    if !arg.transaction_grouper.empty?
+      content_tag(:span) do
+        image_tag arg.transaction_grouper.first.picture.variant(resize_to_limit: [500, 500]), class: 'group-image'
+      end
+    else
+      image_tag '/assets/ungrouped.png', class: 'group-image'
+    end
+  end
 end
