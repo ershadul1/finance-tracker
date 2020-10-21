20d06431f4611e9a7215a81414482771996b5e22
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-21 17:52:58 +0600

    Change default group picture

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
-  has_one_attached :picture, dependent: :destroy
 
   has_many :groups_transactions, foreign_key: :transaction_grouper_id
   has_many :grouped_transactions, through: :groups_transactions
 
+  has_one_attached :picture, dependent: :destroy
   after_commit :add_default_picture, on: %i[create update]
 
   validates :picture, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
@@ -15,7 +15,7 @@ class Group < ApplicationRecord
   def add_default_picture
     return if picture.attached?
 
-    picture.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-image.jpg')),
-                   filename: 'default.jpg', content_type: 'image/jpg')
+    picture.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-group.png')),
+                  filename: 'default-group.png', content_type: 'image/png')
   end
 end
