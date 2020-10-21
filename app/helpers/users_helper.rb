5794c80add8fb7327a5721333c186c5c10d4f18a
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-21 09:21:41 +0600

    Add helper method for user image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_image(arg)
+    if !arg.avatar.attached?
+      image_tag arg.avatar.variant(resize_to_limit: [1000, 1000])
+    else
+      image_tag '/assets/default-user.jpg'
+    end
+  end
 end
