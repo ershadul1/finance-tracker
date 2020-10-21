9b074ecd9849c16f09eb7e373f529ef34a97d84f
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-21 17:53:20 +0600

    Update image helper methods

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,11 +1,9 @@
 module GroupsHelper
   def group_image(arg)
     if arg.picture.attached?
-      content_tag(:span) do
-        image_tag arg.picture.variant(resize_to_limit: [500, 500]), class: 'group-image'
-      end
+      image_tag arg.picture.variant(resize_to_limit: [500, 500]), class: 'group-image'
     else
-      image_tag '/assets/default-image.jpg', class: 'group-image'
+      image_tag("/assets/default-group.png", :alt => "group-image", class: 'group-image')
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def user_image(arg)
-    if !arg.avatar.attached?
+    if arg.avatar.attached?
       image_tag arg.avatar.variant(resize_to_limit: [1000, 1000])
     else
       image_tag '/assets/default-user.jpg'
