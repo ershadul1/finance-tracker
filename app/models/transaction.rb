3b8399d27563a2d3ca2bb542047163ec0e09462d
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-21 08:31:40 +0600

    Add scopes for simplifying transaction queries

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   validates :name, presence: true, length: { in: 2..50 }
-  validates :amount, presence: true, numericality: { less_than: 1_000_000_000 }
+  validates :amount, presence: true, numericality: { less_than: 1_000_000_000, greater_than_or_equal_to: 0 }
   belongs_to :author, class_name: 'User'
 
   has_many :groups_transactions, foreign_key: :grouped_transaction_id
@@ -8,4 +8,10 @@ class Transaction < ApplicationRecord
 
   scope :total_spent, -> { where(earning: false).sum(:amount) }
   scope :total_earned, -> { where(earning: true).sum(:amount) }
+  scope :balance, -> { total_earned - total_spent }
+
+  scope :group_spent, ->(user_id) { where('author_id = ?', user_id).total_spent }
+  scope :group_earned, ->(user_id) { where('author_id = ?', user_id).total_earned }
+
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
